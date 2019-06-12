21b40b7823e5c759050f9f17f01efcc67e38c341
Jarod Aerts <t-jaaert@microsoft.com>
2019-06-12 16:16:41 -0700

    Webhooks

--- app/src/App.js
+++ app/src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Testing the github webhooks stuff. Hopefully things work.
         </a>
       </header>
     </div>
