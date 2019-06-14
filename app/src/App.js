1e60211268c23f0edb9ae890b6daa8e4e4235f1c
Jarod Aerts <t-jaaert@microsoft.com>
2019-06-14 16:55:43 -0700

    sdgsdg

--- app/src/App.js
+++ app/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Something was accomplished.
+          Things were done.
         </p>
         <a
           className="App-link"
