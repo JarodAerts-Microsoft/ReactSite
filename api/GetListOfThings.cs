e2f6ae5c3eee178b60e3cc5c91bfba2fd48e0964
Jarod Aerts <t-jaaert@microsoft.com>
2019-07-02 14:49:54 -0700

    Changed function

--- api/GetListOfThings.cs
+++ api/GetListOfThings.cs
@@ -7,6 +7,7 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace StaticSite.Functions
 {
@@ -17,17 +18,13 @@ namespace StaticSite.Functions
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            List<string> names = new List<string>{
+                "Jarod",
+                "Hannah",
+                "Kevin"
+                };
 
-            string name = req.Query["name"];
-
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            return name != null
-                ? (ActionResult)new OkObjectResult($"Hello, {name}")
-                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+            return (ActionResult)new OkObjectResult(names);
         }
     }
 }
