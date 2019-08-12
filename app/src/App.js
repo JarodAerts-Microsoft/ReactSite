2d0ce427329885cfd8a6ccd7a1d2a0e32dddeeef
Jarod Aerts <t-jaaert@microsoft.com>
2019-08-12 10:12:26 -0700

    Chagnes

--- app/src/App.js
+++ app/src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React.
+          Everything is fine.
         </a>
         <a 
           className="App-link"
