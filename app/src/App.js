2f62c157bed181acd7e78fff2c0460650e36f5a9
Jarod Aerts <t-jaaert@microsoft.com>
2019-08-12 09:54:01 -0700

    Changed a thing

--- app/src/App.js
+++ app/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Testing push on another branch. Doidfddfg more testing
+          Look at this really cool spinny thing we have here.
         </p>
         <a
           className="App-link"
