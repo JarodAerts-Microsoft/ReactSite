fa416ef058dc0261e6019e07cc0cdab0ec79862b
Jarod Aerts <t-jaaert@microsoft.com>
2019-08-12 09:54:23 -0700

    Changed a function

--- api/GetList/index.js
+++ api/GetList/index.js
@@ -2,6 +2,6 @@ module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     context.res = {
-        body: ["Jarod", "Hannah", "Kevin"]
+        body: ["Jarod", "Hannah", "Kevin", "Mitch"]
     }
 };
