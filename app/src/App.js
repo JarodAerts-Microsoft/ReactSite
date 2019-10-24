1505d4b7c3982fe2547f1c25d8c761d2629c2e16
Mitali Karmarkar <mikarmar@microsoft.com>
2019-10-23 17:08:58 -0700

    Update App.js

--- app/src/App.js
+++ app/src/App.js
@@ -22,7 +22,7 @@ function App() {
           className="App-link"
           href="https://authentication.mitalikarmarkar.net/.auth/login/facebook?post_login_redirect_uri=serene-evergreen-1262eeae.mitalikarmarkar.net"
         >
-        Log in with Facebook - Mitali change - see the build system works - 10/23 1:21pm!
+        Log in with Facebook - Mitali change - see the build system works - 10/23 5:08pm!
         </a>
 
       </header>
