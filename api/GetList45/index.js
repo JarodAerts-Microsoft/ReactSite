879de37e03af55e5e0fb4f7cb655d68c61fc715e
Mitali Karmarkar <mikarmar@microsoft.com>
2019-11-12 12:19:39 -0800

    back to one function

--- api/GetList45/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
-
-    context.res = {
-        body: ["Jarod", "Hannah", "Kevin", "Mitch","Colby"]
-    }
-};
