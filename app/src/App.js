5cb5ad9231c414f31a649cd45e844ebe1f90fa18
Mitali Karmarkar <mikarmar@microsoft.com>
2019-11-25 11:48:48 -0800

    a

--- app/src/App.js
+++ app/src/App.js
@@ -68,7 +68,7 @@ class App extends React.Component {
             path="/"
             render={(renderProps) => (
               <div>
-                Home is underneath me on 11/21 2:58pm
+                Home is underneath me on 11/25 11:47am
                 <Home {...this.props} {...renderProps} />
               </div>
             )} />
