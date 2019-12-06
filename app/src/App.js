5c35d6d5e9a4df7184ed366a4d4475198584bb1a
Mitali Karmarkar <mikarmar@microsoft.com>
2019-12-05 18:23:31 -0800

    revert for screenshot

--- app/src/App.js
+++ app/src/App.js
@@ -9,73 +9,73 @@ import {
   Switch
 } from 'react-router-dom'
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Look at this really cool spinny thing we have here.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Everything is fine.
-//         </a>
-//         <a 
-//           className="App-link"
-//           href="https://authentication.mitalikarmarkar.net/.auth/login/facebook?post_login_redirect_uri=serene-evergreen-1262eeae.mitalikarmarkar.net"
-//         >
-//         Log in with Facebook - Mitali change - see the build system works - 11/8 10:12am!
-//         </a>
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Look at this really cool spinny thing we have here.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Everything is fine.
+        </a>
+        <a 
+          className="App-link"
+          href="https://authentication.mitalikarmarkar.net/.auth/login/facebook?post_login_redirect_uri=serene-evergreen-1262eeae.mitalikarmarkar.net"
+        >
+        Log in with Facebook - Mitali change - see the build system works - 11/8 10:12am!
+        </a>
 
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-// const Home = () => (<div><h1>Welcome home</h1></div>)
-const Home = () => (<div><h1>Welcome MITALI</h1><Link to='/about'>Go to about</Link></div>)
-const About = ({ name }) => (<div><h1>About {name}</h1></div>)
-
-class App extends React.Component {
-  render() {
-    return (
-      <Router>
-        <Switch>
-          <Route
-            path="/about"
-            render={(renderProps) => (
-              <div>
-                <Link to='/about/ari'>Ari</Link>
-                <Link to='/about/nate'>Nate</Link>
-                <Route
-                  path="/about/:name"
-                  render={(renderProps) => (
-                    <div>
-                      <About name={renderProps.match.params.name} />
-                      <Link to='/'>Go home</Link>
-                    </div>
-                  )} />
-              </div>
-            )} />
-          <Route
-            path="/"
-            render={(renderProps) => (
-              <div>
-                Home is underneath me on 12/5 4:53pm
-                <Home {...this.props} {...renderProps} />
-              </div>
-            )} />
-        </Switch>
-      </Router>
-    )
-  }
+      </header>
+    </div>
+  );
 }
 
 export default App;
+
+// const Home = () => (<div><h1>Welcome home</h1></div>)
+// const Home = () => (<div><h1>Welcome MITALI</h1><Link to='/about'>Go to about</Link></div>)
+// const About = ({ name }) => (<div><h1>About {name}</h1></div>)
+
+// class App extends React.Component {
+//   render() {
+//     return (
+//       <Router>
+//         <Switch>
+//           <Route
+//             path="/about"
+//             render={(renderProps) => (
+//               <div>
+//                 <Link to='/about/ari'>Ari</Link>
+//                 <Link to='/about/nate'>Nate</Link>
+//                 <Route
+//                   path="/about/:name"
+//                   render={(renderProps) => (
+//                     <div>
+//                       <About name={renderProps.match.params.name} />
+//                       <Link to='/'>Go home</Link>
+//                     </div>
+//                   )} />
+//               </div>
+//             )} />
+//           <Route
+//             path="/"
+//             render={(renderProps) => (
+//               <div>
+//                 Home is underneath me on 12/5 4:53pm
+//                 <Home {...this.props} {...renderProps} />
+//               </div>
+//             )} />
+//         </Switch>
+//       </Router>
+//     )
+//   }
+// }
+
+// export default App;
