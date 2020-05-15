1b2568f6cc0b8acf13e82e67b5051ffbee38c2cf
mkarmark <mikarmar@microsoft.com>
2020-05-14 23:18:13 -0700

    Create Home.js

---
+++ app/src/Home.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function Home() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Welcome to my test site! This site was last modified on 5/14/20 11:17 PM
+        </p>
+        <a
+          className="App-link"
+          href="/shoppingList/"
+        >
+          Let's try some functions!
+        </a>
+        <a 
+          className="App-link"
+          href="/about/"
+        >
+        Gated content
+        </a>
+		        <a 
+          className="App-link"
+          href="/.auth/login/facebook"
+        >
+        Log in with Facebook
+        </a>
+
+      </header>
+    </div>
+  );
+}
+
+export default Home;
