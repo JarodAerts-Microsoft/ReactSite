b32f1a7813bc220a040290866a6f26366dd9ffe8
mkarmark <mikarmar@microsoft.com>
2020-06-09 16:32:17 -0700

    Update index.js

--- api/GetList/index.js
+++ api/GetList/index.js
@@ -25,6 +25,6 @@ module.exports = async function (context, req) {
 
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: JSON.stringify(shoppingList)
+        body: req.headers
     };
 };
