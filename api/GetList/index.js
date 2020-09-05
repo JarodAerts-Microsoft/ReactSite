8c6bab624ae323035f9d284c2d0aec85b8861d4e
mkarmark <mikarmar@microsoft.com>
2020-09-05 14:21:03 -0700

    Update index.js

--- api/GetList/index.js
+++ api/GetList/index.js
@@ -2,7 +2,7 @@ module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     context.res = {
-        headers: { 'foo1': 'baz'},
+        headers: { 'foo2': 'baz'},
         body: {'Joe', 'Misha', 'Mitali', 'Wenonah'}
     }
 };
