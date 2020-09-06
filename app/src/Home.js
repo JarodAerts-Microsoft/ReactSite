aac57e2bb1cf08c8bd237156d0602ef5089e44c9
mkarmark <mikarmar@microsoft.com>
2020-09-05 20:49:28 -0700

    Update Home.js

--- app/src/Home.js
+++ app/src/Home.js
@@ -8,7 +8,7 @@ function Home() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to my hackathon site!
+          Welcome to my hackathon site! -- UPDATED
         </p>
         <a
           className="App-link"
