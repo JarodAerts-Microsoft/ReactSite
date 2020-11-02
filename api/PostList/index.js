c871ec3a5ea985ade43fa835a2d39ff4dfb80272
Mitali Karmarkar <mikarmar@microsoft.com>
2020-11-02 11:33:54 -0800

    new post function

---
+++ api/PostList/index.js
@@ -0,0 +1,8 @@
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    context.res = {
+        headers: { 'foo8': 'baz'},
+        body: req.headers
+    }
+};
