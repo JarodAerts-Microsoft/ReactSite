1e67e9624c12c6028e5f1bfaa2204310c4456a1d
Mitali Karmarkar <mikarmar@microsoft.com>
2020-12-21 15:29:59 -0800

    remove functions

--- api/GetList/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
-
-    context.res = {
-        headers: { 'foobar': 'baz'},
-        body: req.headers
-    }
-};
